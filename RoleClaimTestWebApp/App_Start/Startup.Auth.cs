1de04047a556b3806032be90a3d4293bda7f83f2
Gaurav Sharma <getsetgo_007@msn.com>
2016-04-24 13:44:50 +0530

    First version added from local dev box.

---
+++ RoleClaimTestWebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Web.Helpers;
+using System.Security.Claims;
+
+namespace RoleClaimTestWebApp
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.GivenName;
+            var app1 = new OpenIdConnectAuthenticationOptions
+            {
+                ClientId = "<<your client ID>>", // client Id that you get for your app in AAD
+                Authority = "https://login.microsoftonline.com/<<your tenant>>.onmicrosoft.com", // replace this with your tenant
+                PostLogoutRedirectUri = "https://localhost:44300/", // this is the URL where we will be redirected after successful login
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    RedirectToIdentityProvider = notification =>
+                    {
+                        notification.ProtocolMessage.Prompt = "select_account"; // this is just to show user selection prompt always. Helps during dev.
+                        return Task.FromResult(0);
+                    }
+                },
+                RedirectUri = "https://localhost:44300", // where to receive the token ,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    RoleClaimType = "roles",
+                }
+            };
+
+            app.UseOpenIdConnectAuthentication(app1);
+        }
+    }
+}
---
+++ RoleClaimTestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace RoleClaimTestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = User.IsInRole("approver") ? "Hey! welcome approver." : "Hey! welcome non approver.";
+            return View();
+        }
+
+        [Authorize(Roles = "approver")]
+        public ActionResult Contact()
+        {
+            string userfirstname = ClaimsPrincipal.Current.FindFirst(ClaimTypes.GivenName).Value;
+            ViewBag.Message = String.Format("Welcome, {0}!", userfirstname);
+
+            return View();
+        }
+    }
+}
